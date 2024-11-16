0c4f03cf8a323937490d499f184bba82f0917e4c
quiet <iwhsbomg2@gmail.com>
2024-11-16 16:14:34 +0300

    first .py
    first code, was so proud of it. sep 6 2024.

---
+++ first.py
@@ -0,0 +1,14 @@
+f="failed"
+print("type 'enter' to start")
+a=input()
+if a == 'enter' :
+    print("enter the password")
+else :
+    print(f)
+    e
+p=input()
+if p == '33' :
+    print("success.. ig")
+else :
+    print(f)
+    e
