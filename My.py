9bc6e3908b3630efc7a61ff51db328bd2c0bc497
quiet <iwhsbomg2@gmail.com>
2024-11-16 16:18:09 +0300

    second code.
    second ever code, sep 8 2024

---
+++ My.py
@@ -0,0 +1,21 @@
+print("write (start) to start")
+cond=input()
+if cond == 'start' :
+    print("welcome")
+else:
+    print("no.")
+    end
+print("enter password")
+passs=input()
+if passs == '1' :
+    print("welcome back.")
+else:
+    print("try again.")
+    end
+print("ready to start? (yes,no)")
+condd=input()
+if condd == 'yes' :
+    print("alright! ^_^")
+else:
+    print("alright.")
+    end
